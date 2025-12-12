78c420fb98d698a9593bfb2c96663ff0823649cd
coxteen <costin.ghiujan@gmail.com>
2025-12-12 17:38:15 +0200

    commit

---
+++ src/HCI.AIAssistant.API/Controllers/AIAssistantController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+
+using HCI.AIAssistant.API.Models.DTOs.AIAssistantController;
+using HCI.AIAssistant.API.Services;
+using HCI.AIAssistant.API.Models.DTOs;
+using System.Text;
+
+namespace HCI.AIAssistant.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AIAssistantController : ControllerBase
+{
+    private readonly ISecretsService _secretsService;
+    private readonly IAppConfigurationsService _appConfigurationsService;
+    private readonly IAIAssistantService _aIAssistantService;
+    private readonly IParametricFunctions _parametricFunctions;
+
+    public AIAssistantController(
+        ISecretsService secretsService,
+        IAppConfigurationsService appConfigurationsService,
+        IAIAssistantService aIAssistantService,
+        IParametricFunctions parametricFunctions
+    )
+    {
+        _secretsService = secretsService;
+        _appConfigurationsService = appConfigurationsService;
+        _aIAssistantService = aIAssistantService;
+        _parametricFunctions = parametricFunctions;
+    }
+
+    [HttpPost("message")]
+    [ProducesResponseType(typeof(AIAssistantControllerPostMessageResponseDTO), 200)]
+    [ProducesResponseType(typeof(ErrorResponseDTO), 400)]
+    public async Task<ActionResult> PostMessage([FromBody] AIAssistantControllerPostMessageRequestDTO request)
+    {
+        if (!_parametricFunctions.ObjectExistsAndHasNoNullPublicProperties(request))
+        {
+            return BadRequest(
+                new ErrorResponseDTO()
+                {
+                    TextErrorTitle = "AtLeastOneNullParameter",
+                    TextErrorMessage = "Some parameters are null/missing.",
+                    TextErrorTrace = _parametricFunctions.GetCallerTrace()
+                }
+            );
+        }
+
+        string messageToSendToAssistant = "Instruction: " + _appConfigurationsService.Instruction + "\nMessage: " + request.TextMessage;
+#pragma warning disable CS8604
+        string textMessageResponse = await _aIAssistantService.SendMessageAndGetResponseAsync(messageToSendToAssistant);
+#pragma warning restore CS8604
+
+        AIAssistantControllerPostMessageResponseDTO response = new()
+        {
+            TextMessage = textMessageResponse
+        };
+
+        string? ioTHubConnectionString = _secretsService?.IoTHubSecrets?.ConnectionString;
+        if (ioTHubConnectionString != null)
+        {
+            var serviceClientForIoTHub = ServiceClient.CreateFromConnectionString(ioTHubConnectionString);
+            var seralizedMessage = JsonConvert.SerializeObject(textMessageResponse);
+
+            var ioTMessage = new Message(Encoding.UTF8.GetBytes(seralizedMessage));
+            await serviceClientForIoTHub.SendAsync(_appConfigurationsService.IoTDeviceName, ioTMessage);
+        }
+
+        return Ok(response);
+    }
+}
